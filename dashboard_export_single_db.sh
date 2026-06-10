5bd612b79fccd37f2cdf8ed4cbdda8915f6d8e0e
misiugodfrey <misiug@nvidia.com>
2026-06-10 16:24:34 -0700

    chore: add NVIDIA SPDX headers to 8 source files
    Directly inserts NVIDIA SPDX license headers into files that did not
    receive them in the previous commit (verify-copyright only updates files
    that already have some form of copyright header). Part of org-wide SPDX
    header rollout (see heavydb-internal#8287).

--- dashboard_export_single_db.sh
+++ dashboard_export_single_db.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 
 # Steps
 # 1 - Define a place to export everything
--- sendFiles.sh
+++ sendFiles.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 
 # Assign the first argument as the SOURCE
 SOURCE="$1"
